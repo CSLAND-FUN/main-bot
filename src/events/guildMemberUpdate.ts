de394cea3998496b9ab9dbb258a040a78af1c829
unqxl <andreyzero123@gmail.com>
2022-12-18 21:30:36 +0500

    fix: prevent users set tag if they're not in clan

--- src/events/guildMemberUpdate.ts
+++ src/events/guildMemberUpdate.ts
@@ -2,6 +2,7 @@ import { Event } from "@src/classes/Event";
 
 import { GuildMember } from "discord.js";
 import DiscordBot from "@src/classes/Discord";
+import { ClanMember } from "@modules/clans";
 
 export = class GuildMemberUpdateEvent extends Event {
   constructor() {
@@ -15,7 +16,30 @@ export = class GuildMemberUpdateEvent extends Event {
   ) {
     const old_isInClan = await client.clans.isInClan(oldMember.id);
     const new_isInClan = await client.clans.isInClan(newMember.id);
-    if (!old_isInClan || !new_isInClan) return;
+    if (!old_isInClan || !new_isInClan) {
+      const clans = await client.clans.getClans();
+      for (const clan of clans) {
+        const clan_member = await client.clans
+          .sql<ClanMember>("clans_members")
+          .select()
+          .where({
+            id: newMember.id,
+          })
+          .finally();
+
+        const tag = `[${clan.tag}] `;
+        if (
+          !oldMember.displayName.includes(tag) &&
+          newMember.displayName.includes(tag) &&
+          !clan_member.length
+        ) {
+          const nickname = newMember.displayName.replace(tag, "");
+          return await newMember.edit({
+            nick: nickname,
+          });
+        }
+      }
+    }
 
     const clan = await client.clans.getUserClan(newMember.id);
 
